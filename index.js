2937da08a38fd55863cc3ac694ac6ec0e7901a80
Greg Laabs <OverloadUT@gmail.com>
2015-07-02 00:47:20 -0700

    Proof of concept for playing media! That was a pain to figure out. Everything is still a mess.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').load();
+var dice = require('clj-fuzzy').metrics.dice;
 var alexa = require('alexa-app');
 var app = new alexa.app('plex');
 var plexAPI = require('plex-api');
@@ -22,6 +23,12 @@ app.launch(function(request,response) {
     response.say("Plex is ready");
 });
 
+app.error = function(error, request, response) {
+    console.log(error);
+    response.say("There was an error in the Plex App.");
+    response.send();
+};
+
 app.intent('OnDeckIntent', function(request,response) {
     plex.query('/library/onDeck').then(function(result) {
         var shows = [];
@@ -30,11 +37,12 @@ app.intent('OnDeckIntent', function(request,response) {
             shows.push(result._children[i].grandparentTitle);
         }
 
+        var showsPhraseHyphenized = buildNaturalLangList(shows, 'and', true);
         var showsPhrase = buildNaturalLangList(shows, 'and');
 
         //console.log(result);
 
-        response.say("On deck you've got " + showsPhrase + '.');
+        response.say("On deck you've got " + showsPhraseHyphenized + '.');
         response.card('Plex', showsPhrase + '.', 'On Deck');
         response.send();
     });
@@ -42,32 +50,231 @@ app.intent('OnDeckIntent', function(request,response) {
     return false; // This is how you tell alexa-app that this intent is async.
 });
 
-//app.intent('StartShowIntent', function(request,response) {
-//    plex.query('/library/metadata/4899').then(function(result) {
-//        console.log(result);
-//
-//        response.say("Placeholder");
-//        response.card('Plex', showsPhrase + '.', 'StartShowIntent');
-//        response.send();
-//    });
-//
-//    return false; // This is how you tell alexa-app that this intent is async.
-//});
+app.intent('ShowInfoIntent', function(request,response) {
+    plex.query('/library/metadata/4357').then(function(result) {
+        console.log(result);
 
-function buildNaturalLangList(items, finalWord) {
+        response.say("Temp probably");
+        response.card('Plex', '', 'ShowInfoIntent');
+        response.send();
+    });
+
+    return false; // This is how you tell alexa-app that this intent is async.
+});
+
+app.intent('StartShowIntent', function(request,response) {
+    // If a show and episode was specified: play it
+    // If a show was specified, check if there are any unwatched
+    //   If unwatched exists, play oldest one
+    //   If unwatched does not exist, play random one (ask to confirm?)
+
+    var showName = request.slot('showName', null);
+
+    if(showName) {
+        // Get all TV shows
+        plex.query('/library/sections/1/all').then(function(libraryResults) {
+            //console.log(shows);
+
+            var show = findBestMatch(showName, libraryResults._children, function(show) {
+                return show.title
+            });
+
+            plex.query('/library/metadata/' + show.ratingKey + '/allLeaves').then(function(showResults) {
+                //console.log(showResults);
+                if(show.viewedLeafCount >= show.leafCount) {
+                    // We've seen them all, so pick a random one
+                    episode = showResults._children[randomInt(0, showResults._children.length-1)];
+                    console.log(episode);
+                    playMedia(episode.key, process.env.PLEXCLIENT_NAME, function(err, success) {
+                        if(err || !success) {
+                            response.say("Error: " + err);
+                            response.send();
+                        } else {
+                            response.say("Enjoy this episode from Season " + episode.parentIndex + ": " + episode.title);
+                            response.card('Plex', 'Playing ' + show.title + ': ' + episode.title, 'StartShowIntent');
+                            response.send();
+                        }
+                    });
+                } else {
+                    // Play the next unwatched episode
+                    var episode;
+
+                    for(i=0; i<showResults._children.length; i++) {
+                        if(episode === undefined && !('viewCount' in showResults._children[i])) {
+                            episode = showResults._children[i];
+                        } else if(
+                            !('viewCount' in showResults._children[i])
+                            && showResults._children[i].parentIndex <= episode.parentIndex
+                            && showResults._children[i].index < episode.index
+                        ){
+                            episode = showResults._children[i];
+                        }
+                    }
+
+                    if(!episode) {
+                        response.say("Error: Couldn't find the first unwatched episode");
+                        response.send();
+                    } else {
+                        //console.log(episode);
+                        playMedia(episode.key, process.env.PLEXCLIENT_NAME, function(err, success) {
+                            if(err || !success) {
+                                response.say("Error: " + err);
+                                response.send();
+                            } else {
+                                response.say("Enjoy the next episode of " + show.title + ": " + episode.title);
+                                response.card('Plex', 'Playing ' + show.title + ': ' + episode.title, 'StartShowIntent');
+                                response.send();
+                            }
+                        });
+
+                    }
+                }
+            });
+        }).catch(function(err) {
+            response.say("Error: " + err);
+            response.send();
+        })
+    } else {
+        // TODO ask for which show
+        response.say("No show specified");
+        response.send();
+    }
+
+
+    //playMedia(4905, process.env.PLEXCLIENT_NAME, function(err, success) {
+    //    if(err) {
+    //        response.say("There was an error trying to start your show");
+    //        response.card('Plex', '', 'StartShowError');
+    //        response.send();
+    //    } else {
+    //        response.say("Enjoy the show!");
+    //        response.card('Plex', '', 'StartShowIntent');
+    //        response.send();
+    //    }
+    //});
+
+    return false; // This is how you tell alexa-app that this intent is async.
+});
+
+function findBestMatch(phrase, items, mapfunc) {
+    var bestmatch = {index: -1, score: -1};
+    for(i=0; i<items.length; i++) {
+        var item = items[i];
+        if (mapfunc) {
+            item = mapfunc(items[i]);
+        }
+
+        var score = dice(phrase, item);
+
+        //console.log(score + ': ' + item);
+
+        if(score > bestmatch.score) {
+            bestmatch.index = i;
+            bestmatch.score = score;
+        }
+    }
+
+    return items[bestmatch.index];
+}
+
+function getShow(showName) {
+
+}
+
+function playMedia(mediaKey, client, callback) {
+    // We need the server machineIdentifier for the final playMedia request
+    getMachineIdentifier(function(err, serverMachineIdentifier) {
+        if(err) {
+            callback(new Error('Error getting server Machine Identifier'));
+        } else {
+            // We need the client's address. Could skip this entire call if we already had it
+            // TODO see about having the IP address already on hand
+            getClient('Aphrodite', function(err, client) {
+                clientIP = client.address;
+
+                var keyURI = encodeURIComponent('/library/metadata/' + mediaKey);
+                // Yes, there is a double-nested URI encode here. Wacky Plex API!
+                var libraryURI = encodeURIComponent('library://' + plex.getIdentifier() + '/item/' + keyURI);
+
+                // To play something on a client, we need to add it to a new "Play Queue"
+                plex.postQuery('/playQueues?type=video&uri='+libraryURI+'&shuffle=0').then(function(result) {
+
+                    var playQueueID = result.playQueueID;
+                    var containerKeyURI=encodeURIComponent('/playQueues/' + playQueueID + '?own=1&window=200');
+
+                    // Finally, the call to actually start playing the video
+                    plex.perform('/system/players/'+clientIP+'/playback/playMedia?key=' + keyURI + ' +' +
+                        '&offset=0'+
+                        '&machineIdentifier=' + serverMachineIdentifier+
+                        //'&address=' + process.env.PMS_HOSTNAME + // Address and port aren't needed. Leaving here in case that changes...
+                        //'&port=' + process.env.PMS_PORT +
+                        '&protocol=http'+
+                        '&containerKey=' + containerKeyURI +
+                        '&commandID=1'+
+                        ''
+                    ).then(function() {
+                        callback(null, true);
+                    }).catch(function(error) {
+                        callback(new Error('Error executing the client playMedia request'));
+                    });
+                }).catch(function(error) {
+                    callback(new Error('Error executing the playQueues POST request'));
+                })
+            });
+        }
+    });
+}
+
+function getClient(clientname, callback) {
+    plex.find("/clients", {name: clientname}).then(function(clients) {
+        var clientMatch;
+
+        if (Array.isArray(clients)) {
+            clientMatch = clients[0];
+        }
+
+        callback(null, clientMatch);
+    }).catch(function(error) {
+        callback(error);
+    });
+}
+
+function getMachineIdentifier(callback) {
+    if(!process.env.PMS_IDENTIFIER) {
+        plex.query('/').then(function(res) {
+            process.env.PMS_IDENTIFIER = res.machineIdentifier;
+            callback(null, process.env.PMS_IDENTIFIER);
+        }).catch(function(error) {
+            callback(error);
+        });
+    } else {
+        callback(null, process.env.PMS_IDENTIFIER);
+    }
+}
+
+function buildNaturalLangList(items, finalWord, hyphenize) {
     var output = '';
     for(var i = 0; i<items.length; i++) {
+        var item = items[i];
+        if(hyphenize) {
+            item = item.replace(/ /g, '-');
+        }
+
         if(i === 0) {
-            output += items[i];
+            output += item;
         } else if (i < items.length-1) {
-            output += ', ' + items[i];
+            output += ', ' + item;
         } else {
             if(items.length > 2) {
                 output += ',';
             }
-            output += ' ' + finalWord + ' ' + items[i];
+            output += ' ' + finalWord + ' ' + item;
         }
     }
 
     return output;
+}
+
+function randomInt(low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
 }
--- test/main.js
+++ test/main.js
@@ -45,40 +45,94 @@ describe('Main handler basic functionality', function () {
 
 describe('Intents', function () {
     var lambda = require('../index.js');
-    //var plex;
-    //var plexQueryStub;
     var plexQuerySpy;
+    var plexPostQuerySpy;
+    var plexPerformSpy;
 
     beforeEach(function() {
         //plex = new plexAPI('localhost');
         //plexQueryStub = sinon.stub(plex, 'query')
         //    .resolves('a');
         plexQuerySpy = sinon.spy(lambda.plex, 'query');
+        plexPostQuerySpy = sinon.spy(lambda.plex, 'postQuery');
+        plexPerformSpy = sinon.spy(lambda.plex, 'perform');
     });
 
     afterEach(function() {
         plexQuerySpy.restore();
+        plexPostQuerySpy.restore();
+        plexPerformSpy.restore();
     });
 
-    it('should handle OnDeck Intent', function (done) {
-        var request = JSON.parse(JSON.stringify(requestTemplate)); // Yes this is slow but hey it's just tests?
-        request.request.type = 'IntentRequest';
-        request.request.intent.name = 'OnDeckIntent';
+    describe('OnDeckIntent', function() {
+        it('should handle OnDeck Intent', function (done) {
+            var request = JSON.parse(JSON.stringify(requestTemplate)); // Yes this is slow but hey it's just tests?
+            request.request.type = 'IntentRequest';
+            request.request.intent.name = 'OnDeckIntent';
 
-        lambda.handler(request, {
-            succeed: function(res) {
-                console.log(res);
-                expect(res.response.shouldEndSession).to.be.true;
-                expect(res).to.have.deep.property('response.card.subtitle')
-                    .that.matches(/on deck/i);
-                expect(res).to.have.deep.property('response.outputSpeech.text')
-                    .that.matches(/on deck/i);
-                expect(plexQuerySpy).to.have.been.calledOnce;
-                done();
-            }, fail: function(res) {
-                console.log(res);
-                done(Error('Lambda returned fail()'));
-            }
+            lambda.handler(request, {
+                succeed: function(res) {
+                    //console.log(res);
+                    expect(res.response.shouldEndSession).to.be.true;
+                    expect(res).to.have.deep.property('response.card.subtitle')
+                        .that.matches(/on deck/i);
+                    expect(res).to.have.deep.property('response.outputSpeech.text')
+                        .that.matches(/on deck/i);
+                    expect(plexQuerySpy).to.have.been.calledOnce;
+                    done();
+                }, fail: function(res) {
+                    console.log(res);
+                    done(Error('Lambda returned fail()'));
+                }
+            });
+        });
+    });
+
+    describe('ShowInfoIntent', function() {
+        it('should handle ShowInfo Intent', function (done) {
+            var request = JSON.parse(JSON.stringify(requestTemplate)); // Yes this is slow but hey it's just tests?
+            request.request.type = 'IntentRequest';
+            request.request.intent.name = 'ShowInfoIntent';
+
+            lambda.handler(request, {
+                succeed: function(res) {
+                    //console.log(res);
+                    expect(res.response.shouldEndSession).to.be.true;
+                    expect(res).to.have.deep.property('response.card.subtitle')
+                        .that.matches(/ShowInfoIntent/i);
+                    expect(res).to.have.deep.property('response.outputSpeech.text');
+                    expect(plexQuerySpy).to.have.been.calledOnce;
+                    done();
+                }, fail: function(res) {
+                    console.log(res);
+                    done(Error('Lambda returned fail()'));
+                }
+            });
+        });
+    });
+
+    describe('StartShowIntent', function() {
+        it('should handle StartShow Intent', function (done) {
+            var request = JSON.parse(JSON.stringify(requestTemplate)); // Yes this is slow but hey it's just tests?
+            request.request.type = 'IntentRequest';
+            request.request.intent.name = 'StartShowIntent';
+            request.request.intent.slots.showName = {name: 'showName', value: 'dreadful'};
+
+            lambda.handler(request, {
+                succeed: function(res) {
+                    console.log(res);
+                    expect(res.response.shouldEndSession).to.be.true;
+                    expect(res).to.have.deep.property('response.card.subtitle')
+                        .that.matches(/StartShowIntent/i);
+                    expect(res).to.have.deep.property('response.outputSpeech.text');
+                    expect(plexPerformSpy).to.have.been.calledWithMatch(/playMedia/i);
+                    expect(plexPostQuerySpy).to.have.been.calledWithMatch(/playQueues/i);
+                    done();
+                }, fail: function(res) {
+                    console.log(res);
+                    done(Error('Lambda returned fail()'));
+                }
+            });
         });
     });
 
